44f017267515eb26ae3124ef0c62eda49b51d119
Alex Rose <alex@falconcctv.com>
2022-05-20 01:22:21 +0900

    some progress

--- 2021/2021-3.py
+++ 2021/2021-3.py
@@ -1,7 +1,7 @@
 import os
 
 #print(os.getcwd())
-with open('2021\\input-3.txt','r') as f:
+with open('2021\\input-3a.txt','r') as f:
 	logs = [d.rstrip() for d in f.readlines()]
 #print(len(logs))
 mpop = []
@@ -10,11 +10,22 @@ for i in range(len(logs[0])):
 #	print('i',i,sep='\n')
 	
 	mpop.append(round(sum(int(n[i]) for n in logs)/len(logs)))
+	# print((sum(int(n[i]) for n in logs)/len(logs)))
 
 lpops = ''.join(str(int(not i)) for i in mpop)
 
 mpops = ''.join(str(i) for i in mpop)
 
+
+
 print(mpops,lpops)
 
-print(int(mpops,2)*int(lpops,2))
+for i,b in enumerate(mpops):
+	for n in logs[:]:
+		if n[i] != b:
+			print(f'removing {n} because the {i}th digit isn\'t {b} \t now there are {len(logs)-1} left')
+			logs.remove(n)
+			
+		else: print(f'Keeping {n} because the {i}th digit is {int(not b)}')
+
+print(logs)
