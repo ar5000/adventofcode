61574f640db23e7309bade86e74dfb36d779847e
Alex Rose <alex@falconcctv.com>
2022-06-07 04:17:29 +0900

    input is mostly wrangled

--- 2021/2021-4.py
+++ 2021/2021-4.py
@@ -1,12 +1,26 @@
 import os
 import numpy as np
 from pprint import pprint
+from itertools import chain
 
 #print(os.getcwd())
-with open('2021\\input-4b.txt','r') as f:
+with open('2021\\input-4.txt','r') as f:
 	game = [d.rstrip() for d in f.readlines()]
 
-pprint(game)
+# pprint(game)
 
 draws = [*map(int,game[0].split(','))] # working
 
+
+
+# pprint(marked)
+
+boards = list(chain.from_iterable([item.split(' ') for item in game[1:]]))
+
+board_count = int((len(boards)-boards.count(''))/25)
+
+marked = np.zeros((board_count,5,5),dtype=int)
+boards = np.array([int(item) for item in boards if item]).reshape(board_count,5,5)
+
+pprint(boards)
+pprint(marked)
