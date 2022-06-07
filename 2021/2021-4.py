0ad237249c4ca6edadc01f90133b4d59c93a0693
Alex Rose <alex@falconcctv.com>
2022-06-08 01:42:17 +0900

    making progress

--- 2021/2021-4.py
+++ 2021/2021-4.py
@@ -4,7 +4,7 @@ from pprint import pprint
 from itertools import chain
 
 #print(os.getcwd())
-with open('2021\\input-4.txt','r') as f:
+with open('2021\\input-4b.txt','r') as f:
 	game = [d.rstrip() for d in f.readlines()]
 
 # pprint(game)
@@ -23,4 +23,26 @@ marked = np.zeros((board_count,5,5),dtype=int)
 boards = np.array([int(item) for item in boards if item]).reshape(board_count,5,5)
 
 pprint(boards)
+# pprint(marked)
+
+print(draws)
+
+for num in draws[:]:
+	i = np.where(boards == num)
+	print(f'num= {num}')
+	pprint(i)
+
+	marked[i] = 1
+
+	for b,board in np.ndenumerate(marked):
+		if marked[b[0:2]].sum() == 5:
+			pprint(b)
+			pprint(marked[b[0:2]])
+			print("bingo")
+			pprint(boards[board])
+			pprint(marked)
+			exit()
+
+	
+
 pprint(marked)
