6b40a2ce04092e4c360ce15d2327288f3771f539
Alex <alex@falconcctv.com>
2022-07-04 23:26:03 +0900

    slightly improved

--- 2021/2021-6.py
+++ 2021/2021-6.py
@@ -1,16 +1,30 @@
+from gettext import find
 import numpy as np
 import time
 
 lfs = np.fromfile("2021/input6b.txt", dtype=int, count=-1, sep=',')
-days = 256
+days = 80
 
 for day in range(1,days+1):
+    start = time.time()
     lfs -= 1
+    sub1 = time.time()
     inlabor = (lfs == -1)
-    babies = np.full((lfs[np.where(lfs == -1)].size),8)
+    findbabies = time.time()
+    # babies = np.full((lfs[np.where(lfs == -1)].size),8)
+    babies = np.full((lfs[np.where(inlabor)].size),8)
+    makebabies = time.time()
     lfs[(lfs == -1)] = 6
+    resetmoms = time.time()
     lfs = np.append(lfs,babies)
-    print(f'day {day}')#: {lfs}')
+    add2lfs = time.time()
+    print(f'day {day}: {lfs.size}\tNew babies: {babies.size}')#: {lfs}')
+    print(f'Subtract 1: {sub1-start}')
+    print(f'Find Babies: {findbabies-sub1}')
+    print(f'Make babies: {makebabies-findbabies}')
+    print(f'Reset Moms: {resetmoms-makebabies}')
+    print(f'Add babies to lfs: {add2lfs-resetmoms}\n\n\n')
+
 
 print(f'Total LF: {lfs.size}')
 # print(lfs.size)
