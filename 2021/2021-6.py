936bc3c88802ee948ce2cc39cd76bcec7f0e76bd
Alex <alex@falconcctv.com>
2022-07-04 23:04:28 +0900

    day 6 working, but running slow

--- 2021/2021-6.py
+++ 2021/2021-6.py
@@ -1,1 +1,16 @@
 import numpy as np
+import time
+
+lfs = np.fromfile("2021/input6b.txt", dtype=int, count=-1, sep=',')
+days = 256
+
+for day in range(1,days+1):
+    lfs -= 1
+    inlabor = (lfs == -1)
+    babies = np.full((lfs[np.where(lfs == -1)].size),8)
+    lfs[(lfs == -1)] = 6
+    lfs = np.append(lfs,babies)
+    print(f'day {day}')#: {lfs}')
+
+print(f'Total LF: {lfs.size}')
+# print(lfs.size)
